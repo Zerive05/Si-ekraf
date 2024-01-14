47e4a82fbdd91f92315e32c4d2512285a7baf2f4
Zerive05 <bgsman05@gmail.com>
2024-01-14 13:30:28 +0700

    insert img to database

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -6,7 +6,9 @@ use App\Models\Produk;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 use App\Http\Resources\ProdukResource;
+use Illuminate\Support\Facades\Storage;
 
 class ProdukController extends Controller
 {
@@ -31,12 +33,28 @@ class ProdukController extends Controller
             'stok' => 'required',
             'hargap' => 'required',
             'hargaj' => 'required',
+            'file' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048', // Add image validation rules
         ]);
 
+        $filename = $this->generateRandomString();
+
+        if ($request->hasFile('file')) {
+            $extension = $request->file('file')->extension();
+
+            // Store the file using the hashed filename
+            $path = $request->file('file')->storeAs('gambar', $filename . '.' . $extension);
+
+            // Save the path to the database
+            $request['gambar'] = $path;
+        }
+
+        $request['gambar'] = $filename . '.' . $extension;
         $request['id_penjual'] = Auth::user()->id;
         $prod = Produk::create($request->all());
+
         return new ProdukResource($prod->loadMissing('uploader:id,nama'));
     }
+
 
     public function update(Request $request, $id)
     {
@@ -60,5 +78,16 @@ class ProdukController extends Controller
         $prod->delete();
 
         return new ProdukResource($prod->loadMissing('uploader:id,nama'));
+    }
+
+    function generateRandomString($length = 30)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+        return $randomString;
     }
 }
